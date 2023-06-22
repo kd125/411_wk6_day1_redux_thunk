791da7fece2f278094a4f45b2c6b158235a1678c
Kd125 <kashdangar1250@gmail.com>
2023-06-21 22:28:35 -0500

    final commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Container,
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
+import "./import.css";
 
 const Import = (props) => {
-    // fill out this component
+  const [makeId, setmakeId] = useState(null);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
 
-export default Import
+  const handleMenuClick = (event, makeId) => {
+    console.log("makeId: ", makeId);
+    setMenuAnchorEl(event.currentTarget);
+    setmakeId(makeId);
+  };
+
+  const handleMenuClose = () => {
+    setMenuAnchorEl(null);
+  };
+  console.log("props: ", props);
+  return (
+    <div className="import-container">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={props.fetchMakes}
+        className="import-button"
+      >
+        Import
+      </Button>
+      <h2>Count: {props.makes.length}</h2>
+      <Container className="table-container">
+        <Table style={{ width: 1200 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Make</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.makes.map((make) => (
+              <TableRow key={make.MakeId}>
+                <TableCell>{make.MakeId}</TableCell>
+                <TableCell>{make.MakeName}</TableCell>
+                <TableCell>
+                  <Button
+                    aria-controls="actions-menu"
+                    aria-haspopup="true"
+                    onClick={(e) => handleMenuClick(e, make.MakeId)}
+                  >
+                    <MoreVert />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+      <Menu
+        id="actions-menu"
+        anchorEl={menuAnchorEl}
+        open={Boolean(menuAnchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={() => props.deleteMake(makeId)}>Delete</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default Import;
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,20 @@
 // fill out this container
+import { connect } from "react-redux";
+import Import from "../components/Import";
+import state from "../redux/state";
+import { fetchMakes, deleteMake } from "../redux/actions";
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMakes: () => dispatch(fetchMakes()),
+    deleteMake: (makeId) => dispatch(deleteMake(makeId)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import);
